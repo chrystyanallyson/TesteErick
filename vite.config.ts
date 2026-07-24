87f72b163dbe7dce2ab21e2825c05d99139a5af6
Chrystyan Allyson <chrystyanallyson@MacBook-Air-de-Chrystyan.local>
2026-07-24 10:59:47 -0300

    Update vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/TesteErick/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
